0b119c9dfada75aad77a57df1ccee48d6d93a359
Wylie Fisher <wylie@dukeofcheese.com>
2018-04-18 11:31:33 -0400

    no more styles.

--- src/components/heading/index.js
+++ src/components/heading/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
-import styles from './_index.css';
+import './_index.css';
 
 class Heading extends Component {
   render() {
     return (
-    	<h1 className={styles.Heading}>How Are You Feeling?</h1>
+    	<h1 className={Heading}>How Are You Feeling?</h1>
     );
   }
 }
--- src/components/wrap/index.js
+++ src/components/wrap/index.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 import Heading from '../heading';
 import List from '../list';
 
-import styles from './_index.css';
+import './_index.css';
 
 class Wrap extends Component {
   render() {
     const { children } = this.props;
     return (
-      <div className={styles.Wrap}>
+      <div className={Wrap}>
         <Heading />
         <List />
         {children}
