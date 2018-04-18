4f115c3ff875145d7853313e02991b435e847c35
Wylie Fisher <wylie@dukeofcheese.com>
2018-04-18 17:34:33 -0400

    trying something

--- src/components/wrap/index.js
+++ src/components/wrap/index.js
@@ -8,11 +8,12 @@ import './_index.css';
 class Wrap extends Component {
   render() {
     const { children } = this.props;
+    const subHeadtext = "I Feel" + <span> </span> + ".";
     return (
       <div className="Wrap">
         <Heading level="1" className="heading" children="How Are You Feeling?" />
         <List />
-        <Heading level="2" className="subheading" children="I Feel ________." />
+        <Heading level="2" className="subheading" children={subHeadtext} />
         {children}
       </div>
     );
